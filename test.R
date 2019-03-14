e3ba42438b57fe277d64d594613cc62ba9f6cbff
erikmnielsen <erik_mnielsen@hotmail.com>
2019-03-14 22:12:22 +0100

    test 2

---
+++ test.R
@@ -0,0 +1,6 @@
+> library(readxl)
+> Real_time_oil_market_data_set_06_2018with_Nowcasts <- read_excel("Real-time-oil-market-data-set-06_2018with-Nowcasts.xlsx", 
+                                                                   +     sheet = "WTI")
+#sdsdsdasdasdas
+
+#cdasccc
