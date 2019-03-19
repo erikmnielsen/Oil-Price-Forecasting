907e92d4e0814522f48084f79b03726a7dac75af
Jess Holstein <jess-holstein@hotmail.com>
2019-03-19 18:17:33 +0100

    ts oil

---
+++ Projekt_Oil.R
@@ -0,0 +1,22 @@
+library(readxl)
+library(forecast)
+library(dplyr)
+library(timeSeries)
+library(vars)
+library(aTSA)
+
+Variable <- read_excel("~/8.semester projekt/Variable.xlsx")
+
+
+
+Oil <- ts(Variable[,2:6], start = c(1973,1), frequency = 12)
+
+plot.ts(Oil)
+adf.test(Oil[,1])
+adf.test(Oil[,2])
+adf.test(Oil[,3])
+adf.test(Oil[,4])
+adf.test(Oil[,5])
+
+
+d_WTI<-diff(Oil[,1])
