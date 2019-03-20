5a88b4b1ead908bcbab5c0f9144480122350e8b6
erikmnielsen <erik_mnielsen@hotmail.com>
2019-03-20 12:41:46 +0100

    test

--- Projekt_Oil.R
+++ Projekt_Oil.R
@@ -6,7 +6,17 @@ library(vars)
 library(aTSA)
 
 # Variable <- read_excel("~/8.semester projekt/Variable.xlsx")
-Variable <- read_excel("Variable.xlsx")
+data <- read_excel("Variable.xlsx")
+data <- as.data.frame(data)
+data$Date <- as.yearmon(format(data$Date), "%Y-%m-%d") #"Q%q%Y"
+
+data.xts <- xts(data[-1], data[[1]])
+data.xts <- data.xts["2000/2005-06"]
+plot(data.xts$WTI)
+
+
+
+# test --------------------------------------------------------------------
 
 
 
@@ -18,6 +28,10 @@ adf.test(Oil[,2])
 adf.test(Oil[,3])
 adf.test(Oil[,4])
 adf.test(Oil[,5])
+
+
+testt <- ur.df(Oil[,1],type="trend",lags=lag)
+
 
 
 d_WTI<-diff(Oil[,1])
