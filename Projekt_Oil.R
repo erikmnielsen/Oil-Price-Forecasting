4d9b9fa8ceedf033e825aa421247c5e2620bc8c0
emmamunk <emunk15@student.aau.dk>
2019-03-20 11:31:10 +0100

    test

--- Projekt_Oil.R
+++ Projekt_Oil.R
@@ -9,7 +9,6 @@ library(aTSA)
 Variable <- read_excel("Variable.xlsx")
 
 
-
 Oil <- ts(Variable[,2:6], start = c(1973,1), frequency = 12)
 
 plot.ts(Oil)
@@ -19,5 +18,7 @@ adf.test(Oil[,3])
 adf.test(Oil[,4])
 adf.test(Oil[,5])
 
+#dsfsdfsdfsdfsdf
+#sdsdfdfsdsdfsd
 
 d_WTI<-diff(Oil[,1])
