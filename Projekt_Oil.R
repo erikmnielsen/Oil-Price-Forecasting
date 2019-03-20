2d59a0dfb047382ba1087f13a978130cccfd0ad4
erikmnielsen <erik_mnielsen@hotmail.com>
2019-03-20 14:24:15 +0100

    erik

--- Projekt_Oil.R
+++ Projekt_Oil.R
@@ -10,28 +10,23 @@ data <- read_excel("Variable.xlsx")
 data <- as.data.frame(data)
 data$Date <- as.yearmon(format(data$Date), "%Y-%m-%d") #"Q%q%Y"
 
+View(data)
 data.xts <- xts(data[-1], data[[1]])
-data.xts <- data.xts["2000/2005-06"]
-plot(data.xts$WTI)
-
-
+data.xts <- data.xts["1973-01/2018-06"]
+plot(data.xts)
 
 # test --------------------------------------------------------------------
 
+testt <- ur.df(data.xts$WTI,type=c("none", "drift", "trend"))
+testt
+summary(testt)
+adf.test(data.xts$WTI)
+d_WTI<-diff(data.xts$WTI)
+# VAR ---------------------------------------------------------------------
+VARselect(data.xts)        
+VAR <- VAR(data.xts, p=8) 
+VAR
+
+Acoef(VAR) ### [KxK] matrix med koeff
 
 
-Oil <- ts(Variable[,2:6], start = c(1973,1), frequency = 12)
-
-plot.ts(Oil)
-adf.test(Oil[,1])
-adf.test(Oil[,2])
-adf.test(Oil[,3])
-adf.test(Oil[,4])
-adf.test(Oil[,5])
-
-
-testt <- ur.df(Oil[,1],type="trend",lags=lag)
-
-
-
-d_WTI<-diff(Oil[,1])
