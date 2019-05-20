e579a1e7e37661c14952f63e18f050e02eb719f0
erikmnielsen <erik_mnielsen@hotmail.com>
2019-05-20 16:47:41 +0200

    e

--- ARIMA Forecast.R
+++ ARIMA Forecast.R
@@ -60,6 +60,9 @@ data.xts <- data.xts["1973-01/2018-06"]
 lRO = data.xts$lRO["1973-02/2018-06"]
 lRO.ts <- ts(lRO,frequency=12,start=c(1973, 2), end=c(2018, 6))
 
+dlRO = data.xts$l_diff_RO["1973-02/2018-06"]
+dlRO.ts <- ts(dlRO, frequency=12,start=c(1973, 2), end=c(2018, 6))
+
 nc=8
 m_h1=matrix(nrow=182, ncol=nc)
 m_h3=matrix(nrow=182, ncol=nc)
@@ -69,16 +72,15 @@ m_h12=matrix(nrow=182, ncol=nc)
 
 for (j in 1:nc){
   for (i in 0:181) {
-    train = lRO.ts[1:(364+i), ]
+    train = lRO.ts[1:(364+0), ]
     
     if (j==1){arimaf = Arima(train, order=c(1,0,0))}
     if (j==2){arimaf = Arima(train, order=c(12,0,0))}
-    #if (j==3){arimaf = Arima(train, order=c(24,0,0))}
-    # den stopper ved række 77 ved 24 lags
+    if (j==3){arimaf = Arima(train, order=c(24,0,0), method = "CSS")}
     if (j==4){arimaf = Arima(train, order=c(1,0,1))}
     if (j==5){arimaf = Arima(train, order=c(1,1,0))}
     if (j==6){arimaf = Arima(train, order=c(11,1,0))}
-    #if (j==7){arimaf = Arima(train, order=c(23,1,0))}
+    if (j==7){arimaf = Arima(train, order=c(23,1,0), method = "CSS")}
     if (j==8){arimaf = Arima(train, order=c(0,1,1))}
    
     m_h1[i+1,j]=predict(arimaf, n.ahead=1)$pred[1]
@@ -90,184 +92,29 @@ for (j in 1:nc){
 }
 
 
-# gamle loops -----------------------------------------------------------
-m_h1=matrix(nrow=182, ncol=10)
-
-# AR(12) 
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(12,0,0))
-  m_h1[i+1,1]=predict(arimaf, n.ahead=1)$pred[1] #recursive forecast
-}
-
-# AR(24) 
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(24,0,0))
-  m_h1[i+1,5]=predict(arimaf, n.ahead=1)$pred[1] #recursive forecast
-}
-
-# AR() (SIC)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,0,0))
-  m_h1[i+1,3]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-# AR(1) (AIC)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,0,0))
-  m_h1[i+1,4]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-# ARMA(1,1)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,0,1))
-  m_h1[i+1,5]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-# ARIMA(11,1,0)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(11,1,0))
-  m_h1[i+1,6]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-# ARIMA(23,1,0)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(23,1,0))
-  m_h1[i+1,7]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-# ARIMA(,1,0) SIC
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,1,0))
-  m_h1[i+1,8]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-# ARIMA(1,1,0) AIC
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,1,0))
-  m_h1[i+1,9]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-# ARIMA(0,1,1)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(0,1,1))
-  m_h1[i+1,10]=predict(arimaf, n.ahead=1)$pred[1]
-}
-
-
-m_h3=matrix(nrow=182, ncol=10)
-
-#AR(12)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(12,0,0))
-  m_h3[i+1,1]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-#AR(24)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(24,0,0))
-  m_h3[i+1,2]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# AR() (SIC)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,0,0))
-  m_h3[i+1,3]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# AR(1) (AIC)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,0,0))
-  m_h3[i+1,4]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# ARMA(1,1)
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,0,1))
-  m_h3[i+1,5]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# ARIMA(11,1,0) 
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(11,1,0))
-  m_h3[i+1,6]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# ARIMA(23,1,0) 
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(23,1,0))
-  m_h3[i+1,7]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# ARIMA(,1,0) SIC
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,1,0))
-  m_h3[i+1,8]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# ARIMA(1,1,0) AIC
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(1,1,0))
-  m_h3[i+1,9]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-# ARIMA(0,1,1) 
-for (i in 0:181) {
-  train = lRO.ts[1:(364+i), ]
-  arimaf = Arima(train, order=c(0,1,1))
-  m_h3[i+1,10]=predict(arimaf, n.ahead=3)$pred[3]
-}
-
-m2=matrix(nrow=8, ncol=4)
-
-for (i in 1:8) {
-  arimaf.ts = ts(m_h1[1:181,i], frequency=12, start=c(2003, 6), end=c(2018, 6))
-  m2[i,1]=mean((arimaf.ts-test_h1)^2)/mean((RWf_h1.ts-test_h1)^2)
-  m2[i,2]=hit.ratio(y=test_h1,y.hat=arimaf.ts,d=FALSE)
-}
-
-for (i in 1:8) {
-  arimaf.ts = ts(m_h3[1:179,i], frequency=12, start=c(2003, 8), end=c(2018, 6))
-  m2[i,3]=mean((arimaf.ts-test_h3)^2)/mean((RWf_h3.ts-test_h3)^2)
-  m2[i,4]=hit.ratio(y=test_h3,y.hat=arimaf.ts,d=FALSE)
-}
-
-
 # Forecast Accuracy -----------------------------------------------------------------------
 test_h1 = subset(lRO.ts[, "lRO"],start=365,end = 545)
+test_dh1 = subset(dlRO.ts[, "l_diff_RO"],start=365,end = 545)
 test_h3 = subset(lRO.ts[, "lRO"],start=367,end = 545)
-test_h6 = subset(lRO.ts[, "lRO"],start=369,end = 545)
-test_h9 = subset(lRO.ts[, "lRO"],start=371,end = 545)
-test_h12 = subset(lRO.ts[, "lRO"],start=373,end = 545)
+test_dh3 = subset(dlRO.ts[, "l_diff_RO"],start=367,end = 545)
+test_h6 = subset(lRO.ts[, "lRO"],start=370,end = 545)
+test_dh6 = subset(dlRO.ts[, "l_diff_RO"],start=370,end = 545)
+test_h9 = subset(lRO.ts[, "lRO"],start=373,end = 545)
+test_dh9 = subset(dlRO.ts[, "l_diff_RO"],start=373,end = 545)
+test_h12 = subset(lRO.ts[, "lRO"],start=376,end = 545)
+test_dh12 = subset(dlRO.ts[, "l_diff_RO"],start=376,end = 545)
 
 RW = lRO.ts[-c(1:363), "lRO"]
+
 RWf_h1.ts = ts(RW[1:181], frequency=12, start=c(2003, 6), end=c(2018, 6))
 RWf_h3.ts = ts(RW[1:179], frequency=12, start=c(2003, 8), end=c(2018, 6))
-RWf_h6.ts = ts(RW[1:177], frequency=12, start=c(2003, 10), end=c(2018, 6))
-RWf_h9.ts = ts(RW[1:175], frequency=12, start=c(2003, 12), end=c(2018, 6))
-RWf_h12.ts = ts(RW[1:173], frequency=12, start=c(2004, 2), end=c(2018, 6))
+RWf_h6.ts = ts(RW[1:176], frequency=12, start=c(2003, 11), end=c(2018, 6))
+RWf_h9.ts = ts(RW[1:173], frequency=12, start=c(2004, 2), end=c(2018, 6))
+RWf_h12.ts = ts(RW[1:170], frequency=12, start=c(2004, 5), end=c(2018, 6))
 
 
 m=matrix(nrow=nc, ncol=20)
-pw=1
+pw=2
 pt=2
 x=9
 
@@ -277,14 +124,14 @@ for (i in 1:nc) {
     PE.ARIMA=arimaf.ts-test_h1
     PE.RW=RWf_h1.ts-test_h1
     m[i,1]=mean((PE.ARIMA)^2)/mean((PE.RW)^2)
-    m[i,2]=dm.test(PE.ARIMA, PE.RW, alternative = "two.sided", h=1, power=pw)$p.value
+    m[i,2]=dm.test(PE.ARIMA, PE.RW, alternative = "less", h=1, power=pw)$p.value
     m[i,3]=MDirAcc(test_h1, arimaf.ts)
     m[i,4]=eriksPT_test(test_h1, arimaf.ts)[pt]
   } else{
     PE.dVAR=arimaf.ts-test_dh1
     PE.dRW=test_dh1
     m[i,1]=mean((PE.dVAR)^2)/mean((PE.dRW)^2)
-    m[i,2]=dm.test(PE.dVAR, PE.dRW, alternative = "two.sided", h=1, power=pw)$p.value
+    m[i,2]=dm.test(PE.dVAR, PE.dRW, alternative = "less", h=1, power=pw)$p.value
     m[i,3]=MDirAcc(test_dh1, arimaf.ts)
     m[i,4]=eriksPT_test(test_dh1, arimaf.ts)[pt]
   }
@@ -295,27 +142,25 @@ for (i in 1:nc) {
     PE.ARIMA=arimaf.ts-test_h3
     PE.RW=RWf_h3.ts-test_h3
     m[i,5]=mean((PE.ARIMA)^2)/mean((PE.RW)^2)
-    m[i,6]=dm.test(PE.ARIMA, PE.RW, alternative = "two.sided", h=3, power=pw)$p.value
-    #m[i,7]=hit.ratio(y=test_h3,y.hat=arimaf.ts,d=FALSE)
+    m[i,6]=dm.test(PE.ARIMA, PE.RW, alternative = "less", h=3, power=pw)$p.value
     m[i,7]=MDirAcc(test_h3, arimaf.ts)
     m[i,8]=eriksPT_test(test_h3, arimaf.ts)[pt]
   } else{
     PE.dVAR=arimaf.ts-test_dh3
     PE.dRW=test_dh3
     m[i,5]=mean((PE.dVAR)^2)/mean((PE.dRW)^2)
-    m[i,6]=dm.test(PE.dVAR, PE.dRW, alternative = "two.sided", h=3, power=pw)$p.value
-    #m[i,7]=hit.ratio(y=test_dh3,y.hat=arimaf.ts,d=FALSE)
+    m[i,6]=dm.test(PE.dVAR, PE.dRW, alternative = "less", h=3, power=pw)$p.value
     m[i,7]=MDirAcc(test_dh3, arimaf.ts)
     m[i,8]=eriksPT_test(test_dh3, arimaf.ts)[pt]
   }
 }
 for (i in 1:nc) {
-  arimaf.ts = ts(m_h6[1:177,i], frequency=12, start=c(2003, 10), end=c(2018, 6))
+  arimaf.ts = ts(m_h6[1:176,i], frequency=12, start=c(2003, 11), end=c(2018, 6))
   if(i<x){
     PE.ARIMA=arimaf.ts-test_h6
     PE.RW=RWf_h6.ts-test_h6
     m[i,9]=mean((PE.ARIMA)^2)/mean((PE.RW)^2)
-    m[i,10]=dm.test(PE.ARIMA, PE.RW, alternative = "two.sided", h=6, power=pw)$p.value
+    m[i,10]=dm.test(PE.ARIMA, PE.RW, alternative = "less", h=6, power=pw)$p.value
     #m[i,11]=hit.ratio(y=test_h6,y.hat=arimaf.ts,d=FALSE)
     m[i,11]=MDirAcc(test_h6, arimaf.ts)
     m[i,12]=eriksPT_test(test_h6, arimaf.ts)[pt]
@@ -323,43 +168,43 @@ for (i in 1:nc) {
     PE.dVAR=arimaf.ts-test_dh6
     PE.dRW=test_dh6
     m[i,9]=mean((PE.dVAR^2))/ mean((PE.dRW)^2)
-    m[i,10]=dm.test(PE.dVAR, PE.dRW, alternative = "two.sided", h=6, power=pw)$p.value
+    m[i,10]=dm.test(PE.dVAR, PE.dRW, alternative = "less", h=6, power=pw)$p.value
     m[i,11]=MDirAcc(test_dh6, arimaf.ts)
     m[i,12]=eriksPT_test(test_dh6, arimaf.ts)[pt]
   }
 }
 for (i in 1:nc) {
-  arimaf.ts = ts(m_h9[1:175,i], frequency=12, start=c(2003, 12), end=c(2018, 6))
+  arimaf.ts = ts(m_h9[1:173,i], frequency=12, start=c(2004, 2), end=c(2018, 6))
   if(i<x){
     PE.ARIMA=arimaf.ts-test_h9
     PE.RW=RWf_h9.ts-test_h9
     m[i,13]=mean((PE.ARIMA)^2)/mean((PE.RW)^2)
-    m[i,14]=dm.test(PE.ARIMA, PE.RW, alternative = "two.sided", h=9, power=pw)$p.value
+    m[i,14]=dm.test(PE.ARIMA, PE.RW, alternative = "less", h=9, power=pw)$p.value
     m[i,15]=MDirAcc(test_h9, arimaf.ts)
     m[i,16]=eriksPT_test(test_h9, arimaf.ts)[pt]
   } else{
     PE.dVAR=arimaf.ts-test_dh9
     PE.dRW=test_dh9
     m[i,13]=mean((PE.dVAR^2))/ mean((PE.dRW)^2)
-    m[i,14]=dm.test(PE.dVAR, PE.dRW, alternative = "two.sided", h=9, power=pw)$p.value
+    m[i,14]=dm.test(PE.dVAR, PE.dRW, alternative = "less", h=9, power=pw)$p.value
     m[i,15]=MDirAcc(test_dh9, arimaf.ts)
     m[i,16]=eriksPT_test(test_dh9, arimaf.ts)[2]
   }
 }
 for (i in 1:nc) {
-  arimaf.ts = ts(m_h12[1:173,i], frequency=12, start=c(2004, 2), end=c(2018, 6))
+  arimaf.ts = ts(m_h12[1:170,i], frequency=12, start=c(2004, 5), end=c(2018, 6))
   if(i<x){
     PE.ARIMA=arimaf.ts-test_h12
     PE.RW=RWf_h12.ts-test_h12
     m[i,17]=mean((PE.ARIMA)^2)/mean((PE.RW)^2)
-    m[i,18]=dm.test(PE.ARIMA, PE.RW, alternative = "two.sided", h=12, power=pw)$p.value
+    m[i,18]=dm.test(PE.ARIMA, PE.RW, alternative = "less", h=12, power=pw)$p.value
     m[i,19]=MDirAcc(test_h12, arimaf.ts)
     m[i,20]=eriksPT_test(test_h12, arimaf.ts)[pt]
   } else{
     PE.dVAR=arimaf.ts-test_dh12
     PE.dRW=test_dh12
     m[i,17]=mean((PE.dVAR^2))/ mean((PE.dRW)^2)
-    m[i,18]=dm.test(PE.dVAR, PE.dRW, alternative = "two.sided", h=12, power=pw)$p.value
+    m[i,18]=dm.test(PE.dVAR, PE.dRW, alternative = "less", h=12, power=pw)$p.value
     m[i,19]=MDirAcc(test_dh12, arimaf.ts)
     m[i,20]=eriksPT_test(test_dh12, arimaf.ts)[pt]
   }
@@ -377,6 +222,7 @@ m.df = as.data.frame(m, row.names = c("AR(1)", "AR(12)", "AR(24)", "ARMA(1,1)",
                                       "ARIMA(0,1,1)"))
 m.df =round(m.df, digits = 3)
 grid.table(m.df)
+m.df
 
 
 
