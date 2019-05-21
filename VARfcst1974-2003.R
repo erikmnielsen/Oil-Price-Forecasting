79ede65feae6581987202870579d06ec545da278
chrisbirk96 <chris-birk@hotmail.com>
2019-05-21 11:27:24 +0200

    med ny olievariabel og real price

--- VARfcst1974-2003.R
+++ VARfcst1974-2003.R
@@ -83,14 +83,15 @@ nwPT_test=function(Actual,Forecast){
 
 # Data Preparation --------------------------------------------------------
 
-data <- read_excel("VariableHam74.xlsx")
+data <- read_excel("VariableHam74New.xlsx")
 data <- as.data.frame(data)
 #data$CPI <- log(data$`CPI US`) - lag.xts(log(data$`CPI US`),k = 1)
 #data$CPI_d <- diff.xts(data$lRO, differences = 1)
-data$RO <- (data$WTI/(data$`CPI US`/100))
-data$lRO <- log(data$RO)
+data$lRO <- (data$WTI/(data$`CPI US`/100)) ##den hedder lRO, men det er IKKE log. 
+#data$lRO <- log(data$RO)
 data$l_diff_RO <- diff.xts(data$lRO, differences = 1)
-data$OP <- log(data$`World oil prod`) - lag.xts(log(data$`World oil prod`),k = 1) 
+#data$OP <- log(data$`World oil prod`) - lag.xts(log(data$`World oil prod`),k = 1) 
+data$OP <- data$Prod
 data$OI <- (data$`OECD Pet inv`/data$`US Pet inv`)*data$`US crude inv`
 data$dOI <- diff.xts(data$OI,differences = 1)
 data$Date <- as.yearmon(format(data$Date), "%Y-%m-%d") #"Q%q%Y"
